063c259cd0bdc19b000e04431a211ae06cb27f31
Scarlett <scarlettxoliveira@gmail.com>
2023-04-06 15:00:26 -0300

    all done...

--- script.js
+++ script.js
@@ -1,20 +1,72 @@
-const words = 'honey';
+let words;
+let lives = 6;
 const tries = [];
 const successfulTries = [];
 const failedTries = [];
+const reg = /^[ça-z]*$/;
+let arr1 = []
+const arr2 = []
+const INDEX_OF_INVALID_VALUE = -1
 const toCleanStuffs = {
+    btn: document.getElementById('addword'),
+    wordInput: document.getElementById('wordInp'),
     button: document.getElementById('addbtn'),
     input: document.getElementById('inptext'),
     result: document.getElementById('res'),
     trash: document.getElementById('pa'),
     livesCount: document.getElementById('mylives')
 }
-const INDEX_OF_INVALID_VALUE = -1
 
-const reg = /^[ça-z]*$/;
+// To fill target word
+function addToArray() {
+    words = toCleanStuffs.wordInput.value;
+    arr1 = words.split("")
+    if (!reg.test(toCleanStuffs.wordInput.value) || toCleanStuffs.wordInput.value === "") {
+        alert('Invalid value, please try adding a letter');
+        cleanField();
+        return
+    }
+    cleanField();
+    createSpan();
+}
 
+// to add a try in somewhere
+function add() {
+    const answer = toCleanStuffs.input.value;
+    cleanField();
+    if (!reg.test(answer) || (answer.trim() === "")) {
+        alert('Invalid value, please try adding a letter');
+        cleanField();
+        return
+    }
+    if (inTries(answer)) {
+        window.alert('This letter was already tested, please try another one')
+        cleanField();
+        return
+    }
+    tries.push(answer)
+    if (inWords(answer)) {
+        showLetters(answer)
+        isAlive();
+        return
+    }
+    lives--
+    failedTries.push(answer)
+    toCleanStuffs.trash.append(answer)
+    isAlive();
+}
+
+/**
+ * Verify if `letter` is inside of `arr` 
+ * @param {string[]} arr 
+ * @param {string} letter 
+ * @returns {boolean}
+ */
 function checkIfInside(arr, letter) {
-    return arr.indexOf(letter) != INDEX_OF_INVALID_VALUE
+    // const count = arr.filter((element) => letter == element).length // 8 "(número de vezes que `letter` ocorre em `arr`)"
+    // console.log(count)
+    const some = arr.includes(letter)
+    return some
 }
 
 function inTries(char) {
@@ -30,7 +82,7 @@ function inWrongAnswers(char) {
 }
 
 function inWords(char) {
-    return checkIfInside(words, char)
+    return checkIfInside(words.split(''), char)
 }
 
 function isEqual(letter) {
@@ -38,78 +90,44 @@ function isEqual(letter) {
 }
 
 function createSpan() {
-    const letters = words.split("")
-    letters.forEach(letter => {
+    words.split('').forEach(letter => {
+        let dash = document.createElement('div')
         let span = document.createElement('span')
+        toCleanStuffs.result.append(dash)
+        dash.appendChild(span)
         span.textContent = letter
-        toCleanStuffs.result.append(span)
-        span.setAttribute("id", letter)
         span.setAttribute("style", "visibility: hidden")
-        // console.log(toCleanStuffs.input)
-        // showLetters(letter)
+        dash.setAttribute("style", "border-bottom: 1px solid black; margin: 3px; display: inline;");
     });
 }
 
-// function showLetters() {
-//     if (toCleanStuffs.input.value == letters[0] && letters[0].style.visibility == "hidden") {
-//         letter.setAttribute("style", "visibility: visible")
-//         console.log(letter);
-//     }
-// }
-createSpan();
-function add() {
-    if (!reg.test(toCleanStuffs.input.value) || (toCleanStuffs.input.value.trim() === "")) {
-        alert('Invalid value, please try adding a letter');
-        cleanField();
-        return
+function showLetters(trier) {
+    for (const elements of document.querySelectorAll("span")) {
+        if (elements.textContent.includes(trier)) {
+            elements.style.visibility = 'visible';
+            arr2.push(elements.textContent)
+        }
     }
-    if (inTries(toCleanStuffs.input.value)) {
-        window.alert('This letter was already tested, please try another one')
-        cleanField();
-        return
-    }
-    if (toCleanStuffs.input.value == "h") {
-        const some = document.getElementById("h")
-        console.log(some);
-        some.style.visibility = "visible";
-    }
-    tries.push(toCleanStuffs.input.value)
-    const answer = toCleanStuffs.input.value
-    cleanField();
-    if (inWords(answer)) {
-        successfulTries.push(answer)
-        // toCleanStuffs.result.append(answer)
-        isAlive();
-        return
-    }
-    lives--
-    failedTries.push(answer)
-    toCleanStuffs.trash.append(answer)
-    isAlive();
 }
 
 function cleanField() {
     toCleanStuffs.input.value = ""
+    toCleanStuffs.wordInput.value = ""
 }
 
-let lives = 6;
 function isAlive() {
     toCleanStuffs.livesCount.innerHTML = "Chances left:  " + lives;
     if (failedTries.length > 5) {
         lost();
     }
-    if (successfulTries.length === words.length) {
-        return won();
-    }
+    areEqual(arr1, arr2);
 }
 
-function won() {
-    const winner = document.getElementById('notif');
-    winner.innerHTML = "WINNER"
-    document.body.append(winner);
-    changeBackgroundColor("#13D513");
-    return cleanAlltrashes()
-}
+function areEqual(array1, array2) {
+    if (array1.length === array2.length) {
+        won();
+    }
+};
 
 function cleanAlltrashes() {
     const keys = Object.keys(toCleanStuffs)
@@ -120,17 +138,27 @@ function cleanAlltrashes() {
 
     document.body.append(playAgainBtn);
 }
+
 function changeBackgroundColor(color) {
     document.body.style.background = color
 }
 
+function won() {
+    const winner = document.getElementById('notif');
+    winner.innerHTML = "CONGRATS, YOU DON'T SUCK!"
+    document.body.append(winner);
+    changeBackgroundColor("#13D513");
+    return cleanAlltrashes()
+}
+
 function lost() {
     const error = document.getElementById('notif2');
-    error.innerHTML = "LOOSER"
+    error.innerHTML = "CONGRATS, YOU SUCK!"
     document.body.append(error);
     changeBackgroundColor("#B60606");
     return cleanAlltrashes()
 }
+
 const playAgainBtn = document.createElement('button');
 playAgainBtn.innerHTML = "Play Again"
 playAgainBtn.style.width = "50px";
